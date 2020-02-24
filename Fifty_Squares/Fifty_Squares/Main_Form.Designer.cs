0de2f1d4a526574ef9284a76c1de5cfabde24c4c
Gerbut1986 <andriygerbut@gmail.com>
2020-02-25 01:57:16 +0200

    1st commt / WinFormsApp

---
+++ Fifty_Squares/Fifty_Squares/Main_Form.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Fifty_Squares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.reverseBtn = new System.Windows.Forms.Button();
+            this.drawBtn = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.colorBtn.Location = new System.Drawing.Point(230, 9);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(60, 23);
+            this.colorBtn.TabIndex = 7;
+            this.colorBtn.Text = "Color";
+            this.colorBtn.UseVisualStyleBackColor = true;
+            this.colorBtn.Click += new System.EventHandler(this.colorBtn_Click);
+            // 
+            // reverseBtn
+            // 
+            this.reverseBtn.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.reverseBtn.Location = new System.Drawing.Point(295, 9);
+            this.reverseBtn.Name = "reverseBtn";
+            this.reverseBtn.Size = new System.Drawing.Size(64, 23);
+            this.reverseBtn.TabIndex = 6;
+            this.reverseBtn.Text = "Reverse";
+            this.reverseBtn.UseVisualStyleBackColor = true;
+            this.reverseBtn.Click += new System.EventHandler(this.reverseBtn_Click);
+            // 
+            // drawBtn
+            // 
+            this.drawBtn.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.drawBtn.Location = new System.Drawing.Point(161, 9);
+            this.drawBtn.Name = "drawBtn";
+            this.drawBtn.Size = new System.Drawing.Size(64, 23);
+            this.drawBtn.TabIndex = 5;
+            this.drawBtn.Text = "Drawing";
+            this.drawBtn.UseVisualStyleBackColor = true;
+            this.drawBtn.Click += new System.EventHandler(this.drawBtn_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(8, 2);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(503, 492);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 497);
+            this.Controls.Add(this.colorBtn);
+            this.Controls.Add(this.reverseBtn);
+            this.Controls.Add(this.drawBtn);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button colorBtn;
+        private System.Windows.Forms.Button reverseBtn;
+        private System.Windows.Forms.Button drawBtn;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Fifty_Squares/Fifty_Squares/Main_Form.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fifty_Squares
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Clear_PictureBox()
+        {
+            if (pictureBox.Image != null)
+            {
+                pictureBox.Image.Dispose();
+                pictureBox.Image = null;
+            }
+        }
+
+        void ColorSquares()
+        {
+            Clear_PictureBox(); // Сначала очищаем рictureBox
+            Bitmap bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            pictureBox.Image = bitmap;
+            Pen pen = new Pen(Color.Black);
+
+            float x1 = pictureBox.Width / 10;
+            float y1 = pictureBox.Height / 10;
+            float width = pictureBox.Width - (2 * x1);
+            float height = pictureBox.Height - (2 * y1);
+            float x2 = x1 + width;
+            float y2 = y1;
+            float x3 = x2;
+            float y3 = y2 + height;
+            float x4 = x1;
+            float y4 = y3;
+            float p = 0.08f;
+            graphics.DrawRectangle(pen, x1, y1, width, height);
+            for (int i = 0; i < 50; i++) // В цикле, когда рисуем все 50 квадратов
+            {
+                if (i % 2 == 0)   // ..делаем проверку, если переменная цикла "і" парная(тоесть делиться по модулю на 2, и нету остачи)
+                    pen = new Pen(Color.DarkGoldenrod); // ..то устанавливаем перо в какой то цвет, и отрисовываем ним квадрат
+                else pen = new Pen(Color.Blue);         // Иначе(если не парная переменная цикла) , устанавливаем перо в другой цвет
+                x1 = x1 + (x2 - x1) * p;
+                x2 = x2 + (x3 - x2) * p;
+                x3 = x3 + (x4 - x3) * p;
+                x4 = x4 + (x1 - x4) * p;
+                y1 = y1 + (y2 - y1) * p;
+                y2 = y2 + (y3 - y2) * p;
+                y3 = y3 + (y4 - y3) * p;
+                y4 = y4 + (y1 - y4) * p;
+
+                // Отрисовываем каждую сторону квадрата, по указаным координатам:
+                graphics.DrawLine(pen, x1, y1, x2, y2);
+                graphics.DrawLine(pen, x2, y2, x3, y3);
+                graphics.DrawLine(pen, x1, y1, x4, y4);
+                graphics.DrawLine(pen, x4, y4, x3, y3);
+            }
+        }
+        private void drawBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colorBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reverseBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
